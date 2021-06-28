6af06df144323045e8f009f8f494e9e3fd05fb67
suvorovaav <83077878+suvorovaav@users.noreply.github.com>
2021-06-28 16:51:17 +0300

    Hello World with Spring

---
+++ src/main/java/com/example/hwproject/HWprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HWprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HWprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hwproject/HWprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HWprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
